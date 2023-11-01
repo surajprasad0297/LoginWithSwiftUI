4e04c88162508c1f4b44972932694fe428b30d84
Suraj Prasad <suraj.prasad@mobcoder.com>
2023-11-01 10:29:51 +0530

    Initial Commit

---
+++ LoginWithSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LoginWithSwiftUI
+//
+//  Created by Mobcoder Technologies Private Limited on 01/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginWithSwiftUI/LoginWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginWithSwiftUIApp.swift
+//  LoginWithSwiftUI
+//
+//  Created by Mobcoder Technologies Private Limited on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
